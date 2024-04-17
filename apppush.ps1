af9cd61e3e20aac12ac50699b76a6db96fa8f854
Nikitas-git <142219921+Nikitas-git@users.noreply.github.com>
2024-04-17 16:19:40 +0530

    Create apppush.ps1

---
+++ apppush.ps1
@@ -0,0 +1,13 @@
+# App Center CLI version
+$APPCENTER_CLI_VERSION = "latest"
+
+# App Center token
+$APPCENTER_TOKEN = "2aaaec8d6c304eab7ee07e8ca68dcbf47f5b2d76"  # Replace with your actual token
+
+# Install App Center CLI
+npm install -g appcenter-cli@$APPCENTER_CLI_VERSION
+
+# Login to App Center using the provided token
+appcenter login --token $APPCENTER_TOKEN
+
+appcenter distribute release --debug --app nikitabhagat/testandroid --file android\app\build\outputs\apk\release\app-release.apk  --group "testandroidgroup"
