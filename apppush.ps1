d2b321489089b76d6676e4fc69515fd25cc255c7
Nikitas-git <142219921+Nikitas-git@users.noreply.github.com>
2024-04-18 14:08:15 +0530

    Update apppush.ps1

--- apppush.ps1
+++ apppush.ps1
@@ -10,4 +10,4 @@ npm install -g appcenter-cli@$APPCENTER_CLI_VERSION
 # Login to App Center using the provided token
 appcenter login --token $APPCENTER_TOKEN
 
-appcenter distribute release --debug --app nikitab/androidtest --file android\app\build\outputs\apk\release\app-release.apk  --group "androidtestgroup"
+appcenter distribute release --debug --app nikitab/smsapp --file android\app\build\outputs\apk\release\app-release.apk  --group "smsappgroup"
